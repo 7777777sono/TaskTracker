b263e2873b8ee3abf3b6e724fa9576aa957cdcbb
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-07-24 21:11:56 +0900

    taskのコントローラーでログインしているかどうかによって処理を変えた。

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
     # タスク名は、書いてあるか
     validates :name, presence: true
 
-    validates :deadline, deadline_check
+    validate :deadline_check
 
     # 優先度は、1から3
     validates :passward, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
@@ -14,10 +14,10 @@ class Task < ApplicationRecord
     validates :is_complete, inclusion: { in: [true, false] } 
 
     private
-        # 登録日が本日以降かを確認するメソッド
-        def deadline_check
-            if self.deadline < Date.today
-                errors.add(:deadline, "締め切り日は、本日以降に設定してください。")
-            end
+    # 登録日が本日以降かを確認するメソッド
+    def deadline_check
+        if self.deadline < Date.today
+            errors.add(:deadline, "締め切り日は、本日以降に設定してください。")
         end
+    end
 end
