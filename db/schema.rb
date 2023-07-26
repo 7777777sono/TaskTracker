ad019fd95cd6e501542984d4949a63e6d428f36f
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-07-26 20:29:30 +0900

    usersテーブルにemail:stringを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_130251) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_112819) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.date "deadline"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_130251) do
     t.string "google_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
