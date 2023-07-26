e38d32b90434d100a1b0f18d92b4436536759f23
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-07-26 16:36:51 +0900

    フロントエンドにデータを送ることに成功

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.x.cors_allowed_origins = ENV.fetch('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')
   end
 end
