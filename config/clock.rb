3771d727a3e6a7d5ccabf3123bbaeb4349e4db68
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2023-08-23 22:14:29 +0900

    clockworkを使えるようにした

--- config/clock.rb
+++ config/clock.rb
@@ -51,5 +51,9 @@ module Clockwork
     every(1.day, 'delete_expired_task', at: '00:00')
 
     # 毎日7:00, 12:00, 20:00に未完了のタスクを通知する。
-    every(1.day, 'line_send_notify', at: ['07:00', '12:00', '20:00', '22:10'])
+    every(1.day, 'line_send_notify', at: ['07:00', '12:00', '20:00'])
+
+    every(1.day, 'another_job' , at: ['22:20']) do
+        puts "Another job executed every 10 minutes at #{Time.now}"
+    end
 end
